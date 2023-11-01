6fec154a0e6809dc7fac46d8163d4cbfb9233425
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-11-01 21:53:31 +1100

    Added dynamic working and tally display

--- javascript.js
+++ javascript.js
@@ -1,38 +1,47 @@
 addEventListener('DOMContentLoaded', () => {
 
-const displayValue = [];
+let displayValue = [];
 
-
+let clrBtn = document.querySelector('.clrBtn');
+clrBtn.addEventListener('click', () => {
+    console.log('cleared');
+    displayValue = [];
+    numArray = [];
+    operator = 0;
+    displayTally.textContent = "";
+    displayWorking.textContent = 0;
+});  
 
 const input = document.querySelector('.input');
 const operators = document.querySelector('.operators');
 
 const numbers = document.querySelector('.numbers');
 
-//make operators
-for (let i = 0; i < 4; i++) {
-    const btn = document.createElement('div');
-    btn.setAttribute("class", "btn");
-    operators.appendChild(btn);
-};
+// //make operators
+// for (let i = 0; i < 4; i++) {
+//     const btn = document.createElement('div');
+//     btn.setAttribute("class", "btn");
+//     operators.appendChild(btn);
+// };
 
-// make buttons
+// // make buttons
 
-for (let i = 0; i < 9; i++) {
-    const btn = document.createElement('div');
-    btn.setAttribute("class", "btn");
-    numbers.appendChild(btn);
-};
+// for (let i = 0; i < 9; i++) {
+//     const btn = document.createElement('div');
+//     btn.setAttribute("class", "btn");
+//     numbers.appendChild(btn);
+// };
 
 
 let aNum;
 let bNum;
 let operator;
 
-console.log(operate(6, 7, "*"));
 
+displayTally = document.querySelector('.displayTally');
+displayWorking = document.querySelector('.displayWorking');
+displayWorking.textContent = 0;
 
-display = document.querySelector('.display');
 
 // number buttons
 numBtns = document.querySelectorAll('.numBtn');
@@ -40,19 +49,52 @@ numBtns = document.querySelectorAll('.numBtn');
 numBtns.forEach( (e) => { 
     e.addEventListener('click', () => {
         displayValue.push(e.textContent);
-        display.textContent = displayValue.join("");
-        console.log(displayValue);
+        displayWorking.textContent = displayValue.join("");
+        console.log('displayValue', displayValue);
     })
 });
 
 // operator buttons
 
-opBtns = document.querySelectorAll('.opBtns');
+opBtns = document.querySelectorAll('.optBtn');
+
+
+let numArray = [];
+let numArrayIndex = 0;
 
 opBtns.forEach( (e) => {
         e.addEventListener('click', () => {
+            operator = e.textContent;
+
+            console.log('operator', operator);
             // to code
             //on click, get the type of operator.
+
+                       
+
+            if (e.textContent === "=")  {
+                console.log("Calling = function");
+            } else if (displayWorking.textContent ==="Display Working") {
+                console.log("is empty")
+            } else {
+                numArray.push(parseInt(displayValue.join("")));
+                console.log('numArray', numArray);
+                displayValue = [];
+                console.log(displayValue);
+
+                displayTally.textContent = numArray[0] + " " + operator;
+
+                if (numArray[1]) {
+                    console.log(numArray[0], operator, numArray[1]);
+                    let result = operate(numArray[0], numArray[1], operator);
+                    console.log('result = ', result);
+                    displayTally.textContent = result + " " + operator;
+                    numArray = [];
+
+                }
+            }
+            
+          
             // if equals, then call relevant function, and insert value A, value B, etc etc into function
             
             // if operator is '+, -, *, or /' put the value of displayValue into a new variable, 'valueA'
@@ -65,6 +107,14 @@ opBtns.forEach( (e) => {
 
 
 
+function clear() {
+    console.log('cleared');
+    displayValue = [];
+    numArray = [];
+    operator = 0;
+    displayTally.textContent = "";
+    displayWorking.textContent = "";
+};
 
 
 function add(a, b) {
